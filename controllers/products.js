66aa6c5a8de9067126155145bd8a9a1df9b44563
Diebut <guevaradiego_2011@hotmail.com>
2023-06-30 23:55:07 -0300

    se recarga el archivo products de la carpeta controllers

---
+++ controllers/products.js
@@ -0,0 +1,51 @@
+const { products } = require('../db.json');
+
+module.exports = {
+
+    getProducts:(req, res) => {
+        res.json({products})  //products: products})
+    },
+    addProducts: (req, res) => {
+        //const products = req.body.name;
+        const {name} = req.body;
+        products.push({
+            name,
+            id: products.length
+        })
+        res.json({
+            'success':true,
+            'msg': 'successfully added'
+        });
+    },
+
+    updateProducts:(req, res)=>{
+       const { id } = req.params;
+       const { name } = req.body;
+
+       products.forEach((products, i) => {
+            if (products.id === Number(id)) {
+                products.name = name;
+            }
+
+       });
+       res.json({
+            'success':true,
+            'msg': 'update'
+       })
+
+    },
+
+    deleteProducts:(req, res)=>{
+        const {id} = req.params;
+        products.forEach((product, i)=>{
+            if (product.id === Number(id)) {
+                products.splice(i, 1);
+            }
+        });
+        res.json({
+            'success':true,
+            'msg': 'delete'
+       })
+
+    }
+};
